955229e55bda0c93968e07910f6494f364c71283
Thang Kieu <thang.kieu@naustud.io>
2015-06-30 21:38:15 +0700

    add css font login page

--- client/views/shared/header.jsx
+++ client/views/shared/header.jsx
@@ -1,6 +1,29 @@
-
+// header component
 Header = React.createClass({
-  render: function() {
-    return <p>Header</p>;
-  }
+
+	statics: {
+		header: null
+	},
+
+	componentWillMount: function () {
+		var type = this.props.type || 'login';
+
+		switch (type) {
+			case 'login':
+				this.header = (<ul className="header__list">
+								<li><a href="/" className="logo">Cubbr</a></li>
+								<li>Thông tin ?</li>
+							</ul>);
+				break;
+			default:
+		}
+	},
+
+  	render: function() {
+  		var type = this.props.type;
+
+    	return (<header className="header">
+    			{ this.header }
+    		</header>);
+  	}
 });
--- client/views/systems/login.jsx
+++ client/views/systems/login.jsx
@@ -1,21 +1,27 @@
 
 LoginTpt = ReactMeteor.createClass({
-  templateName: 'LoginTpt',
+	templateName: 'LoginTpt',
 
-  startMeteorSubscriptions: function() {
-  },
+	startMeteorSubscriptions: function() {
+	},
 
-  // Make sure your component implements this method.
-  getMeteorState: function() {
-  },
+	// Make sure your component implements this method.
+	getMeteorState: function() {
+	},
 
-  render: function() {
-    return (
-      <div>
-        <Header/>
-        <p>LoginTpt</p>
-        <Footer/>
-      </div>
-    );
-  }
+	render: function() {
+		return (
+			<div className="page-wrapper">
+				<Header/>
+				<section className="page-content-container page-login">
+					<div className="abs-center">
+						<div className="abs-center__content">
+							<a href="" className="login-fb-btn"><i className="fa fa-facebook-square ib vm"></i><span className="ib vm">Facebook</span></a>
+						</div>
+					</div>
+				</section>
+				{/*<Footer/>*/}
+			</div>
+		);
+	}
 });
