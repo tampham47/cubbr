263893fa48060fe206ad56e3964faf004bbf5161
Tam Pham <tampham47@live.com>
2015-07-05 04:18:31 +0700

    integrate history

--- client/views/messages/list.jsx
+++ client/views/messages/list.jsx
@@ -2,12 +2,41 @@ MessagesListTpt = ReactMeteor.createClass({
   templateName: 'MessagesListTpt',
 
   startMeteorSubscriptions: function() {
+    Meteor.subscribe('Users.getByLocation');
+  },
+
+  getInitialState: function() {
+    return {
+      userList: []
+    };
   },
 
   getMeteorState: function() {
+    var userList = Meteor.users.find({}).fetch();
+    return {
+      userList: userList
+    };
   },
 
   render: function() {
+    var userListRender = <p className="wrapper">No users</p>;
+
+    if (this.state.userList.length) {
+      userListRender = this.state.userList.map(function(item, i) {
+        return (
+          <li className="table-view-cell media">
+            <a className="navigate-right" href={"/users/detail/" + item._id} data-transition="slide-in">
+              <img className="media-object pull-left" src="images/thumbnail_48x48.png" alt="Placeholder" />
+              <div className="media-body">
+                {item.fullName}
+                <p>{item.occupation || item.email}</p>
+              </div>
+            </a>
+          </li>
+        );
+      });
+    }
+
     return (
       <div>
         <Header navActive="4" isHeader="true"
@@ -16,87 +45,7 @@ MessagesListTpt = ReactMeteor.createClass({
         <div className="container content">
           <h6 className="small-title">Histories</h6>
           <ul className="table-view table-view-custom user-list">
-            <li className="table-view-cell media">
-              <a className="navigate-right" href="/messages/detail" data-transition="slide-in">
-                <img className="media-object pull-left" src="images/thumbnail_48x48.png" alt="Placeholder" />
-                <div className="media-body">
-                  Tan Nguyen
-                  <p>Javascript Developer</p>
-                </div>
-              </a>
-            </li>
-            <li className="table-view-cell media">
-              <a className="navigate-right" href="/messages/detail" data-transition="slide-in">
-                <img className="media-object pull-left" src="images/thumbnail_48x48.png" alt="Placeholder" />
-                <div className="media-body">
-                  Tam Pham
-                  <p>Javascript Developer</p>
-                </div>
-              </a>
-            </li>
-            <li className="table-view-cell media">
-              <a className="navigate-right" href="/messages/detail" data-transition="slide-in">
-                <img className="media-object pull-left" src="images/thumbnail_48x48.png" alt="Placeholder" />
-                <div className="media-body">
-                  Phuc Kieu
-                  <p>English Teacher</p>
-                </div>
-              </a>
-            </li>
-            <li className="table-view-cell media">
-              <a className="navigate-right" href="/messages/detail" data-transition="slide-in">
-                <img className="media-object pull-left" src="images/thumbnail_48x48.png" alt="Placeholder" />
-                <div className="media-body">
-                  Tan Nguyen
-                  <p>Javascript Developer</p>
-                </div>
-              </a>
-            </li>
-            <li className="table-view-cell media">
-              <a className="navigate-right" href="/messages/detail" data-transition="slide-in">
-                <img className="media-object pull-left" src="images/thumbnail_48x48.png" alt="Placeholder" />
-                <div className="media-body">
-                  Tam Pham
-                  <p>Javascript Developer</p>
-                </div>
-              </a>
-            </li>
-            <li className="table-view-cell media">
-              <a className="navigate-right" href="/messages/detail" data-transition="slide-in">
-                <img className="media-object pull-left" src="images/thumbnail_48x48.png" alt="Placeholder" />
-                <div className="media-body">
-                  Phuc Kieu
-                  <p>English Teacher</p>
-                </div>
-              </a>
-            </li>
-            <li className="table-view-cell media">
-              <a className="navigate-right" href="/messages/detail" data-transition="slide-in">
-                <img className="media-object pull-left" src="images/thumbnail_48x48.png" alt="Placeholder" />
-                <div className="media-body">
-                  Tan Nguyen
-                  <p>Javascript Developer</p>
-                </div>
-              </a>
-            </li>
-            <li className="table-view-cell media">
-              <a className="navigate-right" href="/messages/detail" data-transition="slide-in">
-                <img className="media-object pull-left" src="images/thumbnail_48x48.png" alt="Placeholder" />
-                <div className="media-body">
-                  Tam Pham
-                  <p>Javascript Developer</p>
-                </div>
-              </a>
-            </li>
-            <li className="table-view-cell media">
-              <a className="navigate-right" href="/messages/detail" data-transition="slide-in">
-                <img className="media-object pull-left" src="images/thumbnail_48x48.png" alt="Placeholder" />
-                <div className="media-body">
-                  Phan Kieu
-                  <p>English Teacher</p>
-                </div>
-              </a>
-            </li>
+            {userListRender}
           </ul>
         </div>
       </div>
