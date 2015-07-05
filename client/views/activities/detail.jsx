c279d7c6914a551594e61d3310cfb01911ae3fba
Tan Nguyen <tannd1993@gmail.com>
2015-07-05 07:22:33 +0700

    NOTHING

--- client/views/activities/detail.jsx
+++ client/views/activities/detail.jsx
@@ -2,59 +2,66 @@ ActivDetailTpt = ReactMeteor.createClass({
   templateName: 'ActivDetailTpt',
 
   startMeteorSubscriptions: function() {
+
   },
 
   // Make sure your component implements this method.
   getMeteorState: function() {
+    // console.log(activity);
+    // return {
+    //   activity: {}
+    // };
   },
 
   getInitialState: function () {
-    var user = {};
-    Meteor.call('Users.getCurrent', function (err, result) {
-      console.log(result);
-      user = result;
-    });
-    return user;
+    var activityId = Router.current().params.id;
+    Meteor.call('Activities.getById', activityId, function (err, result) {
+      this.setState({activity: result});
+    }.bind(this));
+
+    return {
+      activity: {}
+    };
   },
 
   render: function() {
+    var activityDetail = '';
+    if (this.state.activity) {
+      item = this.state.activity;
+      activityDetail = (function () {
+        return (
+          <ul className="table-view table-view-comment user-block">
+            <li className="table-view-cell media">
+              <img className="media-object pull-left" src="../../images/thumbnail_64x64.png" alt="Placeholder" />
+              <div className="media-body">
+                <p className="user-fullname">{item.username}</p>
+                <p className="user-occupation">{item.occupation}</p>
+              </div>
+            </li>
+          </ul>
+        );
+      })();
+    }
     return (
       <div>
         <Header navActive="1" isHeader="true"
           plusHref="/activ/new" />
         <div className="container">
-
-          <ul className="table-view table-view-comment user-block">
-            <li className="table-view-cell media">
-              <img className="media-object pull-left" src="../images/thumbnail_64x64.png" alt="Placeholder" />
-              <div className="media-body">
-                <p className="user-fullname">Tan Nguyen</p>
-                <p className="user-occupation">Naustud.io</p>
-              </div>
-            </li>
-          </ul>
-
-          <div className="activ-block wrapper">
-            <p className="activ-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco.
-            </p>
-            <p className="activ-date">*5 hours ago</p>
-          </div>
+          {activityDetail}
 
           <div className="comment-list">
             <h6 className="small-title bold">Comments</h6>
 
             <ul className="table-view table-view-comment">
               <li className="table-view-cell media">
-                <img className="media-object pull-left" src="../images/thumbnail_64x64.png" alt="Placeholder" />
+                <img className="media-object pull-left" src="../../images/thumbnail_64x64.png" alt="Placeholder" />
                 <div className="media-body">
                   <p className="cmt-title">Tan Nguyen</p>
                   <p className="cmt-content">Lorem ipsum dolor sit amet, consectetur</p>
                 </div>
               </li>
               <li className="table-view-cell media">
-                <img className="media-object pull-left" src="../images/thumbnail_64x64.png" alt="Placeholder" />
+                <img className="media-object pull-left" src="../../images/thumbnail_64x64.png" alt="Placeholder" />
                 <div className="media-body">
                   <p className="cmt-title">Tan Nguyen</p>
                   <p className="cmt-content">Lorem ipsum dolor sit amet, consectetur</p>
--- client/views/activities/list.jsx
+++ client/views/activities/list.jsx
@@ -6,8 +6,12 @@ ActivListTpt = ReactMeteor.createClass({
 	},
 
   getInitialState: function() {
+    Meteor.call('Users.getCurrent', function (err, result) {
+      this.setState({currentUser: result});
+    }.bind(this));
     return {
-      activList: []
+      activList: [],
+      currentUser: {}
     };
   },
 
@@ -21,12 +25,21 @@ ActivListTpt = ReactMeteor.createClass({
 
   render: function() {
     var listItem = '';
+    var imageUrl = "../images/thumbnail_64x64.png";
+    var services = this.state.currentUser.services;
+    // console.log('OKIE', services['facebook']);
+    if (this.state.currentUser) {
+      // imageUrl = "http://avatars.io/facebook/" + this.state.currentUser.services.facebook.id + "?size=medium";
+    }
+
+
     if (this.state.activList.length) {
       listItem = this.state.activList.map(function (item, i) {
+        console.log(item);
         return (
           <li className="table-view-cell media">
             <a className="navigate-right" href={"/activ/detail/" + item._id} data-transition="slide-in">
-              <img className="media-object pull-left" src="images/thumbnail_64x64.png" alt="Placeholder" />
+              <img className="media-object pull-left" src={imageUrl} alt="Placeholder" />
               <div className="media-body">
                 {item.username}
                 <p>{item.content}</p>
